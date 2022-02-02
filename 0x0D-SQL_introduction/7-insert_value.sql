7f207a4c13751faec71f556b873536920ca79da7
BuZz <Bekamadawit@gmail.com>
2022-02-02 03:51:02 +0300

    fix

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,2 @@
--- a new row in the table first_table
--- inserts a new row in the table first_table
-INSERT INTO `first_table`(`id`, `name`)
-VALUES(89, "Holberton School");
+-- Inserts a new row into the table first_table.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
